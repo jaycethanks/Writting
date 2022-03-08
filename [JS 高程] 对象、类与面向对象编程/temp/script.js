6e1dde5b5bc51dc3032d649d605b5bf8a83578b5
jayce <jaycethanks@outlook.com>
2022-03-08 22:47:12 +0800

    refresh

--- [JS 高程] 对象、类与面向对象编程/temp/script.js
+++ [JS 高程] 对象、类与面向对象编程/temp/script.js
@@ -3,11 +3,12 @@ function object(o) {
   F.prototype = o;
   return new F();
 }
-function createAnother(original) {
-  let clone = ojbect(original); //通过调用函数创建一个新会县
-  clone.sayHi = function () {
-    //以某种当时增强这个对象
-    console.log("hi");
-  };
-  return clone; // 返回这个对象
-}
+let person = {
+  name: "tom",
+  friends: ["a", "b"],
+};
+let person_A = object(person);
+let person_B = object(person);
+
+person_A.friends.push("c");
+console.log(person_B.friends, "--line14");
